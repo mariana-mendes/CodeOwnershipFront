5659aefd51c488e0fddb9127b72b92f3b898091d
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-10-23 20:55:49 -0300

    sub-components

--- codeownership/src/App.js
+++ codeownership/src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projeto from './Projeto/Projeto';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <h2>Nome do usuário</h2>
       </div>
+
+          {/* <User></User> */}
+          
+        <div>
+       <Projeto></Projeto>
+       </div>
+      </div>
+     
     );
   }
 }
---
+++ codeownership/src/Metric/Metric.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Metric extends Component {
+  render() {
+    return (
+      <div className="Metric">
+        <h4> Metric 1</h4>
+      </div>
+    );
+  }
+}
+
+export default Metric;
