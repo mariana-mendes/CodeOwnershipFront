176fc4f77952d0ff700d5b4cfd0fac3584335a34
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-12-13 17:48:10 -0300

    Telas depara autenticação

--- codeownership/src/App.js
+++ codeownership/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Projects from './Projects/Projects';
 import Project from './Project/Project';
+import Navigation from './Navigation/Navigation';
+import * as ROUTES from './constants/constants';
 import NewUser from './NewUser/NewUser';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import Route  from 'react-router-dom/Route';
+import Route from 'react-router-dom/Route';
 import { Jumbotron, Button, Badge } from 'react-bootstrap';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       projects: [],
@@ -21,40 +23,29 @@ class App extends Component {
 
   componentWillMount() {
     axios.get('http://localhost:4000/user')
-    .then(response => {
-      this.setState({projects: response.data})
-    })
+      .then(response => {
+        this.setState({ projects: response.data })
+      })
   }
 
   render() {
     return (
+
       <Router>
-         {this.state.loggedIn ? 
-          <div>
-            <div className="sidenav">
-              <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" className="avatar"></img>
-              <a>Profile</a>
-              <Link to="/projects" onClick={this.handleClick} >Projects</Link>
-              <a>Log out</a>
-            </div>
-            <Route path="/project/:_id" component={Project}/>,
-            <Route path="/projects" exact strict render={()=> 
-                         (<Projects projects={this.state.projects}/>)}/>  
-          </div> :
-              <Jumbotron >
-                       <legend>  
-                         <Badge color="secondary">Welcome!
-                             Analyse your projects git! </Badge>
-                      </legend>
-                      <Button  onClick={this.toggle} color="primary">Login</Button>{' '}
-                      <Link to="/newUser" color="primary">New User?</Link>
-                      <hr/>
-                      <div>
-                        
-                        <Route path="/newUser" exact strict component={NewUser}/>
-                      </div>
-            </Jumbotron>   }
+        <div>
+          <Navigation/>
+          <hr/>
+          {/* <Route exact path={ROUTES.LANDING} component={LandingPage} /> */}
+          {/* <Route path={ROUTES.SIGN_UP} component={SignUpPage} /> */}
+          <Route path={ROUTES.SIGN_IN} component={NewUser} />
+          {/* <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} /> */}
+          {/* <Route path={ROUTES.HOME} component={HomePage} /> */}
+          {/* <Route path={ROUTES.ACCOUNT} component={AccountPage} /> */}
+          {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
+        </div>
       </Router>
+
+
     );
   }
 
@@ -63,8 +54,8 @@ class App extends Component {
     this.setState({
       loggedIn: !this.state.loggedIn
     })
-}
-  
+  }
+
 }
 
 export default App;
